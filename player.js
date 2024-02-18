cc8867a1b2353b1e74df91477b353655f4d0bdea
2Beast101 <bradendavidc@yahoo.com>
2024-02-18 18:22:35 -0500

    Fixing conflicts

--- player.js
+++ player.js
@@ -1,67 +1,67 @@
-var wizard;                // declare variable that will refer to player object
-var idleAnim;              // declare variable for idle animation
-var runAnim;               // declare variable for run animation
+// var wizard;                // declare variable that will refer to player object
+// var idleAnim;              // declare variable for idle animation
+// var runAnim;               // declare variable for run animation
 
-var movingRight = false;
-var movingLeft = false;
-var movingUp = false;
-var movingDown = false;
-const playerSpeed = 3;
-
-
-// class for player character
-class player {
-    // class methods
-    constructor() {
-        this.sprite = new Sprite(25, 25);
-        this.sprite.addAni(idleAnim);
-    }
-
-    // moves player right by setting velocity
-    moveRight() {
-        this.sprite.vel.x = playerSpeed;
-    }
-
-    // moves player left by setting velocity
-    moveLeft() {
-        this.sprite.vel.x = -playerSpeed;
-    }
-
-    // moves player down by setting velocity
-    moveDown() {
-        this.sprite.vel.y = playerSpeed;
-    }
-
-    // moves player up by setting velocity
-    moveUp() {
-        this.sprite.vel.y = -playerSpeed;
-    }
-
-    // stops player movement by setting velocity to 0
-    stopMovement() {
-        this.sprite.vel.x = 0;
-        this.sprite.vel.y = 0;
-        if (this.sprite.vel.x == 0 && this.sprite.vel.y == 0) {
-            this.sprite.changeAni(idleAnim);
-        }
-    }
-
-    normalizeMovement() {
-        this.sprite.vel.normalize().mult(playerSpeed);
-    }
-
-    get posx() {
-        return this.sprite.x;
-    }
-    get posy() {
-        return this.sprite.y;
-    }
+// var movingRight = false;
+// var movingLeft = false;
+// var movingUp = false;
+// var movingDown = false;
+// const playerSpeed = 3;
 
 
+// // class for player character
+// class player {
+//     // class methods
+//     constructor() {
+//         this.sprite = new Sprite(25, 25);
+//         this.sprite.addAni(idleAnim);
+//     }
 
-    // class attributes
-    sprite;           // player sprite
-}
+//     // moves player right by setting velocity
+//     moveRight() {
+//         this.sprite.vel.x = playerSpeed;
+//     }
+
+//     // moves player left by setting velocity
+//     moveLeft() {
+//         this.sprite.vel.x = -playerSpeed;
+//     }
+
+//     // moves player down by setting velocity
+//     moveDown() {
+//         this.sprite.vel.y = playerSpeed;
+//     }
+
+//     // moves player up by setting velocity
+//     moveUp() {
+//         this.sprite.vel.y = -playerSpeed;
+//     }
+
+//     // stops player movement by setting velocity to 0
+//     stopMovement() {
+//         this.sprite.vel.x = 0;
+//         this.sprite.vel.y = 0;
+//         if (this.sprite.vel.x == 0 && this.sprite.vel.y == 0) {
+//             this.sprite.changeAni(idleAnim);
+//         }
+//     }
+
+//     normalizeMovement() {
+//         this.sprite.vel.normalize().mult(playerSpeed);
+//     }
+
+//     get posx() {
+//         return this.sprite.x;
+//     }
+//     get posy() {
+//         return this.sprite.y;
+//     }
+
+
+
+//     // class attributes
+//     sprite;           // player sprite
+// }
 
 
 
--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+var wizard;                // declare variable that will refer to player object
+var idleAnim;              // declare variable for idle animation
+var runAnim;               // declare variable for run animation
+var i = 0;
+
+const playerSpeed = 3;
+var isMoving = false;
 
 let electricAnim;
 const electricFrames = 20;
@@ -13,8 +20,6 @@ function loadanimations() {
     // loads run animation sprite sheet (strip), and seperates frames
     runAnim = loadAnimation("assets/runAnimSheet.png",
         { frameSize: [32, 32], frames: 6 });
-
-
 
 
     fireballAnim = loadAnimation(
@@ -41,16 +46,70 @@ function preload() {
     loadanimations();
 }
 
+/ class for player character
+class player {
+    // class methods
+    constructor() {
+        this.sprite = new Sprite(25, 25);
+        this.sprite.addAni(idleAnim);
+    }
+
+    // moves player right by setting velocity
+    moveRight() {
+        this.sprite.vel.x = playerSpeed;
+    }
+
+    // moves player left by setting velocity
+    moveLeft() {
+        this.sprite.vel.x = -playerSpeed;
+    }
+
+    // moves player down by setting velocity
+    moveDown() {
+        this.sprite.vel.y = playerSpeed;
+    }
+
+    // moves player up by setting velocity
+    moveUp() {
+        this.sprite.vel.y = -playerSpeed;
+    }
+
+    // stops player movement by setting velocity to 0
+    stopMovement() {
+        this.sprite.vel.x = 0;
+        this.sprite.vel.y = 0;
+        if (this.sprite.vel.x == 0 && this.sprite.vel.y == 0) {
+            this.sprite.changeAni(idleAnim);
+        }
+    }
+
+    normalizeMovement() {
+        this.sprite.vel.normalize().mult(playerSpeed);
+    }
+
+    get posx() {
+        return this.sprite.x;
+    }
+    get posy() {
+        return this.sprite.y;
+    }
 
 
-// // tests movement functions - not sure if this is good enough or not (never done TDD tbh)
-// var i = 0;             // argument for testPlayerMovement()
-// function testPlayerMovement(i) {
-//     if (i < 100) { wizard.moveRight(); };
-//     if (i > 100 && i < 200) { wizard.moveDown(); };
-//     if (i > 200 && i < 300) { wizard.moveLeft(); };
-//     if (i > 300) { wizard.moveUp(); };
-// }
+
+    // class attributes
+    sprite;           // player sprite
+}
+
+
+
+// tests movement functions - not sure if this is good enough or not (never done TDD tbh)
+var i = 0;             // argument for testPlayerMovement()
+function testPlayerMovement(i) {
+    if (i < 100) { wizard.moveRight(); };
+    if (i > 100 && i < 200) { wizard.moveDown(); };
+    if (i > 200 && i < 300) { wizard.moveLeft(); };
+    if (i > 300) { wizard.moveUp(); };
+}
 
 // setup canvas and player sprite
 function setup() {
@@ -65,4 +124,28 @@ function draw() {
     playerMovement();
     wizard.normalizeMovement();
     castSpell();
+    if (kb.presses('right')) {
+        wizard.sprite.mirror.x = false;
+        wizard.sprite.changeAni(runAnim);
+    }
+    if (kb.pressing('right')) { wizard.moveRight(); }
+    if (kb.released('right')) { wizard.stopMovement(); }
+
+    // controls movement left
+    if (kb.presses('left')) {
+        wizard.sprite.mirror.x = true;
+        wizard.sprite.changeAni(runAnim);
+    }
+    if (kb.pressing('left')) { wizard.moveLeft(); }
+    if (kb.released('left')) { wizard.stopMovement(); }
+
+    // controls movement down
+    if (kb.presses('down')) { wizard.sprite.changeAni(runAnim); }
+    if (kb.pressing('down')) { wizard.moveDown(); }
+    if (kb.released('down')) { wizard.stopMovement(); }
+
+    // controls movement up
+    if (kb.presses('up')) { wizard.sprite.changeAni(runAnim); }
+    if (kb.pressing('up')) { wizard.moveUp(); }
+    if (kb.released('up')) { wizard.stopMovement(); }
 }
