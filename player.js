c4926f1e34ee55acfb1ac271342e3c8bf782b33e
2Beast101 <bradendavidc@yahoo.com>
2024-03-07 17:34:25 -0500

    Refactoring

--- player.js
+++ player.js
@@ -1,148 +1,281 @@
 
-const playerSpeed = 3;
-var isMoving = false;
+
+export function makePlayer(p) {
+    return {
+        sprite: new p.Sprite(25, 25, 20, 32),
+        currentAnimation: null,
+        animations: {},
+        speed: 3,
 
 
-// class for player character
-class player {
-    // class methods
-    constructor() {
-        // creates new sprite at 25, 25 with a physical size of 20 x 32 pixels
-        this.sprite = new Sprite(25, 25, 20, 32);
-        this.sprite.addAni(idleAnim);
-        // this.sprite.collider = "none";
-        //this.sprite.debug = true;
-    }
-
-    // moves player right by setting velocity
-    moveRight() {
-
-        this.sprite.vel.x = playerSpeed;
-        //this.sprite.changeAni(runAnim);
-        this.sprite.mirror.x = false;
-
-    }
-
-    // moves player left by setting velocity
-    moveLeft() {
-        this.sprite.vel.x = -playerSpeed;
-        //this.sprite.changeAni(runAnim);
-        this.sprite.mirror.x = true;
-    }
-
-    // moves player down by setting velocity
-    moveDown() {
-        this.sprite.vel.y = playerSpeed;
-    }
-
-    //this.sprite.changeAni(runAnim);
 
 
-    // moves player up by setting velocity
-    moveUp() {
-        this.sprite.vel.y = -playerSpeed;
-    }
-    //this.sprite.changeAni(runAnim);
+        setup() {
+            this.loadAnimations();
+            this.sprite.addAni(this.animations.idle);
+        },
 
-    // stops player movement by setting velocity to 0
-    stopMovementX() {
-        this.sprite.vel.x = 0;
-        if (!this.moving()) {
+        loadAnimations() {
+            this.animations.idle = p.loadAnimation("assets/idleAnimSheet.png",
+                { frameSize: [32, 32], frames: 2 });
+            this.animations.idle.frameDelay = 18;     // slows down idle animation
 
-            this.sprite.changeAni(idleAnim);
-        }
-    }
+            this.animations.run = p.loadAnimation("assets/runAnimSheet.png",
+                { frameSize: [32, 32], frames: 6 });
 
-    normalizeMovement() {
-        this.sprite.vel.normalize().mult(playerSpeed);
-    }
-
-    get posx() {
-        return this.sprite.position.x;
-    }
-    get posy() {
-        return this.sprite.position.y;
-    }
+            this.animations.death = p.loadAnimation("assets/deathAnimSheet.png",
+                { frameSize: [32, 32], frames: 10 });
 
 
-    stopMovementY() {
-        this.sprite.vel.y = 0;
-        if (!this.moving()) {
+        },
 
-            this.sprite.changeAni(idleAnim);
-        }
-    }
+        moveRight() {
+            this.sprite.vel.x = this.speed;
+            this.sprite.mirror.x = false;
+        },
 
-    moving(moving) {
-        if ((this.sprite.vel.y == 0) && (this.sprite.vel.x == 0)) {
-            moving = false;
-        } else {
-            moving = true;
-        }
-        return moving;
-    }
+        moveLeft() {
+            this.sprite.vel.x = -this.speed;
+            //this.sprite.changeAni(runAnim);
+            this.sprite.mirror.x = true;
+        },
 
-    die() {
-        this.health = 0;
-        // Stop all player movement 
-        this.sprite.vel.y = 0;
-        this.sprite.vel.x = 0;
-    
-        // Set the animation to the first frame of the death animation
-        this.sprite.changeAni(deathAnim);
-        this.sprite.animation.frame = 0;
-    
-        // Play the death animation once and once only
-        this.sprite.animation.play();
-        this.sprite.animation.looping = false;
-    }    
+        moveUp() {
+            this.sprite.vel.y = -this.speed;
+        },
 
-    respawn() {
-        this.health = 1;
-        this.sprite.position.set(25, 25);
-        this.sprite.changeAni(idleAnim);  // Change animation to idle when respawning
-    }
+        moveDown() {
+            this.sprite.vel.y = this.speed;
+        },
 
-    // class attributes
-    sprite;           // player sprite
-    health = 1;       // player health - currently operating on assumption that we want 1 hit death
+        normalize() {
+            this.sprite.vel.normalize().mult(this.speed);
+        },
+
+
+        isMoving() {
+            if ((this.sprite.vel.y == 0) && (this.sprite.vel.x == 0)) { return true; } else { return false; };
+        },
+
+
+        stopMovementX() {
+            this.sprite.vel.x = 0;
+            if (!this.isMoving()) {
+                this.sprite.changeAni(this.animations.idle);
+            }
+        },
+
+        stopMovementY() {
+            this.sprite.vel.y = 0;
+            if (!this.isMoving()) {
+                this.sprite.changeAni(this.animations.idle);
+            }
+        },
+
+        movement() {
+            if (p.kb.presses('up')) {
+                this.sprite.changeAni(this.animations.run);
+            };
+            if (p.kb.pressing('up')) {
+                this.moveUp();
+                this.movingVert = true;
+            };
+            if (p.kb.released('up')) {
+                this.stopMovementY();
+                this.movingVert = false;
+            };
+
+
+            if (p.kb.presses('down')) {
+                this.sprite.changeAni(this.animations.run);
+            };
+            if (p.kb.pressing('down')) {
+                this.moveDown();
+                this.movingVert = true;
+            };
+            if (p.kb.released('down')) {
+                this.stopMovementY();
+                this.movingVert = false;
+            };
+
+
+            if (p.kb.presses('right')) {
+                this.sprite.changeAni(this.animations.run);
+            };
+            if (p.kb.pressing('right')) {
+                this.moveRight();
+                this.movingLat = true;
+            };
+            if (p.kb.released('right')) {
+                this.stopMovementX();
+                this.movingLat = false;
+            };
+
+
+            if (p.kb.presses('left')) {
+                this.sprite.changeAni(this.animations.run);
+            };
+            if (p.kb.pressing('left')) {
+                this.moveLeft();
+                this.movingLat = true;
+            };
+            if (p.kb.released('left')) {
+                this.stopMovementX();
+                this.movingLat = false;
+            };
+
+
+            if (this.movingLat && this.movingVert) { this.normalize(); }
+        },
+
+
+
+    };
 }
 
 
 
-function playerMovement() {
 
-    //keeps wizard from moving if they are dead
-    if (wizard.health > 0) {
-        if (kb.presses('right')) {
-            wizard.sprite.mirror.x = false;
-            wizard.sprite.changeAni(runAnim);
-        }
-        if (kb.pressing('right')) { wizard.moveRight(); }
-        if (kb.released('right')) { wizard.stopMovementX(); }
+// // class for player character
+// class player {
+//     // class methods
+//     constructor(p, animation) {
+//         // creates new sprite at 25, 25 with a physical size of 20 x 32 pixels
+//         this.sprite = new p.Sprite(25, 25, 20, 32);
+//         this.sprite.addAni(animation);
+//         // this.sprite.collider = "none";
+//         //this.sprite.debug = true;
+//     }
+
+//     // moves player right by setting velocity
+//     moveRight() {
+
+//         this.sprite.vel.x = playerSpeed;
+//         //this.sprite.changeAni(runAnim);
+//         this.sprite.mirror.x = false;
+
+//     }
+
+//     // moves player left by setting velocity
+//     moveLeft() {
+//         this.sprite.vel.x = -playerSpeed;
+//         //this.sprite.changeAni(runAnim);
+//         this.sprite.mirror.x = true;
+//     }
+
+//     // moves player down by setting velocity
+//     moveDown() {
+//         this.sprite.vel.y = playerSpeed;
+//     }
+
+//     //this.sprite.changeAni(runAnim);
 
 
-        // controls movement left
-        if (kb.presses('left')) {
-            wizard.sprite.mirror.x = true;
-            wizard.sprite.changeAni(runAnim);
-        }
-        if (kb.pressing('left')) { wizard.moveLeft(); }
-        if (kb.released('left')) { wizard.stopMovementX(); }
+//     // moves player up by setting velocity
+//     moveUp() {
+//         this.sprite.vel.y = -playerSpeed;
+//     }
+//     //this.sprite.changeAni(runAnim);
+
+//     // stops player movement by setting velocity to 0
+//     stopMovementX() {
+//         this.sprite.vel.x = 0;
+//         if (!this.moving()) {
+
+//             this.sprite.changeAni(idleAnim);
+//         }
+//     }
+
+//     normalizeMovement() {
+//         this.sprite.vel.normalize().mult(playerSpeed);
+//     }
+
+//     get posx() {
+//         return this.sprite.position.x;
+//     }
+//     get posy() {
+//         return this.sprite.position.y;
+//     }
 
 
-        // controls movement down
-        if (kb.presses('down')) { wizard.sprite.changeAni(runAnim); }
-        if (kb.pressing('down')) { wizard.moveDown(); }
-        if (kb.released('down')) { wizard.stopMovementY(); }
+//     stopMovementY() {
+//         this.sprite.vel.y = 0;
+//         if (!this.moving()) {
 
-        // controls movement up
-        if (kb.presses('up')) { wizard.sprite.changeAni(runAnim); }
-        if (kb.pressing('up')) { wizard.moveUp(); }
-        if (kb.released('up')) { wizard.stopMovementY(); }
+//             this.sprite.changeAni(idleAnim);
+//         }
+//     }
 
-        if (kb.presses('y')) { wizard.die();}
-        // Check if 'r' is pressed to respawn
-        if (kb.presses('r')) { respawnPlayer();}
-    }
-}
+//     moving(moving) {
+//         if ((this.sprite.vel.y == 0) && (this.sprite.vel.x == 0)) {
+//             moving = false;
+//         } else {
+//             moving = true;
+//         }
+//         return moving;
+//     }
+
+//     die() {
+//         this.health = 0;
+//         // Stop all player movement
+//         this.sprite.vel.y = 0;
+//         this.sprite.vel.x = 0;
+
+//         // Set the animation to the first frame of the death animation
+//         this.sprite.changeAni(deathAnim);
+//         this.sprite.animation.frame = 0;
+
+//         // Play the death animation once and once only
+//         this.sprite.animation.play();
+//         this.sprite.animation.looping = false;
+//     }
+
+//     respawn() {
+//         this.health = 1;
+//         this.sprite.position.set(25, 25);
+//         this.sprite.changeAni(idleAnim);  // Change animation to idle when respawning
+//     }
+
+//     // class attributes
+//     sprite;           // player sprite
+//     health = 1;       // player health - currently operating on assumption that we want 1 hit death
+// }
+
+
+
+// function playerMovement() {
+
+//     //keeps wizard from moving if they are dead
+//     if (wizard.health > 0) {
+//         if (kb.presses('right')) {
+//             wizard.sprite.mirror.x = false;
+//             wizard.sprite.changeAni(runAnim);
+//         }
+//         if (kb.pressing('right')) { wizard.moveRight(); }
+//         if (kb.released('right')) { wizard.stopMovementX(); }
+
+
+//         // controls movement left
+//         if (kb.presses('left')) {
+//             wizard.sprite.mirror.x = true;
+//             wizard.sprite.changeAni(runAnim);
+//         }
+//         if (kb.pressing('left')) { wizard.moveLeft(); }
+//         if (kb.released('left')) { wizard.stopMovementX(); }
+
+
+//         // controls movement down
+//         if (kb.presses('down')) { wizard.sprite.changeAni(runAnim); }
+//         if (kb.pressing('down')) { wizard.moveDown(); }
+//         if (kb.released('down')) { wizard.stopMovementY(); }
+
+//         // controls movement up
+//         if (kb.presses('up')) { wizard.sprite.changeAni(runAnim); }
+//         if (kb.pressing('up')) { wizard.moveUp(); }
+//         if (kb.released('up')) { wizard.stopMovementY(); }
+
+//         if (kb.presses('y')) { wizard.die(); }
+//         // Check if 'r' is pressed to respawn
+//         if (kb.presses('r')) { respawnPlayer(); }
+//     }
+// }
--- sketch.js
+++ sketch.js
@@ -1,181 +1,216 @@
-var wizard;                // declare variable that will refer to player object
-var idleAnim;              // declare variable for idle animation
-var runAnim;               // declare variable for run animation
-var emptyAnim;
-var state = 0;             // controls state (title screen, level0, level1 etc)
-var startButton;           // variable for title screen start button
-var level0Drawn = 0;       // variable used in drawing level0
-
-var i = 0; // for movement test func
-
-let electricAnim;
-const electricFrames = 20;
-
-let angleShotAnim;
-
-// Loads all animations for sprites in the project.
-function loadanimations() {
-    // loads idle animation sprite sheet (strip), and seperates frames
-    idleAnim = loadAnimation("assets/idleAnimSheet.png",
-        { frameSize: [32, 32], frames: 2 });
-    idleAnim.frameDelay = 18;     // slows down idle animation
 
 
-    // loads run animation sprite sheet (strip), and seperates frames
-    runAnim = loadAnimation("assets/runAnimSheet.png",
-        { frameSize: [32, 32], frames: 6 });
 
-    //loads death animation sprite sheet and seperates frames
-    deathAnim = loadAnimation("assets/deathAnimSheet.png",
-        { frameSize: [32, 32], frames: 10 });
-
-    emptyAnim = loadAnimation("assets/emptyAnim.png",
-    { frameSize: [32, 32], frames: 1 });
-
-    // loads idle animation for golem enemy
-    golemIdle = loadAnimation("assets/golemIdle.png",
-        { framesize: [16, 16], frames: 4 });
-    golemIdle.frameDelay = 17;
-
-    // loads run animation
-    golemRun = loadAnimation("assets/golemIdle.png",
-        { framesize: [16, 16], frames: 4 });
-    golemRun.frameDelay = 5;
+import { GameState } from "./gamestate.js";
+import { makePlayer } from "./player.js";
 
 
-    fireballAnim = loadAnimation(
-        'assets/fireball/FB001.png',
-        'assets/fireball/FB002.png',
-        'assets/fireball/FB003.png',
-        'assets/fireball/FB004.png'
-    );
-    fireballAnim.frameDelay = 15;
+new p5((p) => {
+    const wizard = new makePlayer(p);
 
-    electricAnim = loadAnimation(
-        "assets/electric/tile1.png",
-        "assets/electric/tile2.png",
-        "assets/electric/tile3.png",
-        "assets/electric/tile4.png"
-    );
-    electricAnim.frameDelay = electricFrames;
+    p.preload = () => {
+        wizard.setup();
+    }
 
-    angleShotAnim = loadAnimation(
-        "assets/tile001.png",
-        "assets/tile002.png",
-        "assets/tile003.png",
-        "assets/tile004.png"
-    );
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight);
+    }
 
-    angleShotAnim.frameDelay = 15;
-    // angleShotAnim.scale = 0.2;
+    p.draw = () => {
+        p.clear();
+        p.background('#fce1b6');
+        wizard.movement();
+    }
 
-}
+    p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+    }
 
-// tests movement functions - not sure if this is good enough or not (never done TDD tbh)
-function testPlayerMovement(i) {
-    if (i < 100) { wizard.moveRight(); };
-    if (i > 100 && i < 200) { wizard.moveDown(); };
-    if (i > 200 && i < 300) { wizard.moveLeft(); };
-    if (i > 300) { wizard.moveUp(); };
-}
 
-// Add a respawn function
-function respawnPlayer() {
-    wizard.health = 1;  // Reset player health
-    wizard.sprite.position.set(25, 25);  // Set the player position to the initial position
-    wizard.sprite.changeAni(idleAnim);  // Play idle animation
-}
+})
 
-function setupButtons() {
-    startButton = new Button({
-        x: width/2, y: height - 50,
-        width: 100, height: 50,
-        align_x: 0, align_y: 0,
-        content: 'Click to Start',
-        on_press() {
-            state++;
-        }
-    })
-}
 
-// preload images for animation - executed once
-function preload() {
-    loadanimations();
-}
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    wizard = new player();
-    setupButtons();
-    createGolemGroup();
-}
 
-// Currently implemented in level0
-// // setup canvas and player sprite - executed once
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var i = 0; // for movement test func
+
+// let electricAnim;
+// const electricFrames = 20;
+
+// let angleShotAnim;
+
+// // Loads all animations for sprites in the project.
+// function loadanimations(p) {
+//     // loads idle animation sprite sheet (strip), and seperates frames
+//     idleAnim = p.loadAnimation("assets/idleAnimSheet.png",
+//         { frameSize: [32, 32], frames: 2 });
+//     idleAnim.frameDelay = 18;     // slows down idle animation
+
+
+//     // loads run animation sprite sheet (strip), and seperates frames
+//     runAnim = p.loadAnimation("assets/runAnimSheet.png",
+//         { frameSize: [32, 32], frames: 6 });
+
+//     //loads death animation sprite sheet and seperates frames
+//     deathAnim = p.loadAnimation("assets/deathAnimSheet.png",
+//         { frameSize: [32, 32], frames: 10 });
+
+//     emptyAnim = p.loadAnimation("assets/emptyAnim.png",
+//         { frameSize: [32, 32], frames: 1 });
+
+//     // loads idle animation for golem enemy
+//     golemIdle = p.loadAnimation("assets/golemIdle.png",
+//         { framesize: [16, 16], frames: 4 });
+//     golemIdle.frameDelay = 17;
+
+//     // loads run animation
+//     golemRun = p.loadAnimation("assets/golemIdle.png",
+//         { framesize: [16, 16], frames: 4 });
+//     golemRun.frameDelay = 5;
+
+
+//     fireballAnim = p.loadAnimation(
+//         'assets/fireball/FB001.png',
+//         'assets/fireball/FB002.png',
+//         'assets/fireball/FB003.png',
+//         'assets/fireball/FB004.png'
+//     );
+//     fireballAnim.frameDelay = 15;
+
+//     electricAnim = p.loadAnimation(
+//         "assets/electric/tile1.png",
+//         "assets/electric/tile2.png",
+//         "assets/electric/tile3.png",
+//         "assets/electric/tile4.png"
+//     );
+//     electricAnim.frameDelay = electricFrames;
+
+//     angleShotAnim = p.loadAnimation(
+//         "assets/tile001.png",
+//         "assets/tile002.png",
+//         "assets/tile003.png",
+//         "assets/tile004.png"
+//     );
+
+//     angleShotAnim.frameDelay = 15;
+//     // angleShotAnim.scale = 0.2;
+
+// }
+
+// // tests movement functions - not sure if this is good enough or not (never done TDD tbh)
+// function testPlayerMovement(i) {
+//     if (i < 100) { wizard.moveRight(); };
+//     if (i > 100 && i < 200) { wizard.moveDown(); };
+//     if (i > 200 && i < 300) { wizard.moveLeft(); };
+//     if (i > 300) { wizard.moveUp(); };
+// }
+
+// // Add a respawn function
+// function respawnPlayer() {
+//     wizard.health = 1;  // Reset player health
+//     wizard.sprite.position.set(25, 25);  // Set the player position to the initial position
+//     wizard.sprite.changeAni(idleAnim);  // Play idle animation
+// }
+
+// function setupButtons() {
+//     startButton = new Button({
+//         x: width / 2, y: height - 50,
+//         width: 100, height: 50,
+//         align_x: 0, align_y: 0,
+//         content: 'Click to Start',
+//         on_press() {
+//             state++;
+//         }
+//     })
+// }
+
+
+
+
+// // preload images for animation - executed once
+// function preload() {
+//     loadanimations();
+// }
+
 // function setup() {
 //     createCanvas(windowWidth, windowHeight);
 //     wizard = new player();
-//     tempSprite = new Sprite();
-//     wizard.sprite.overlaps(tempSprite);
+//     setupButtons();
+//     createGolemGroup();
 // }
 
-var test;
-var respawnState = false; // New variable to track respawn state
 
-function draw() {
-    // state = 0 corresponds to start screen
-    if (state == 0) {
-        background("#5cb8ff");   // arbitrary color choice, can be changed
-        fill(0);
-        textSize(50);
-        textAlign(CENTER);
-        text("The Wizard's Quest", width/2, height/2);
-        startButton.draw();
-        wizard.sprite.changeAni(emptyAnim);
-    }
-    // state = 1 corresponds to level0
-    if (state == 1) {
-        background("#fce1b6");   // arbitrary color choice, can be changed
-        if (level0Drawn == 0) { drawLevel0(); }   // makes sure level sprites only get drawn once - breaks otherwise
-        textAlign(LEFT);
-        text('WASD to move\n' +
-        'Click to attack (mouse to aim)\n' +
-        'Space to shoot fireball sideways\n' +
-        'Press 1 to change attack\n' +
-        'Press b to spawn golem enemy\n' +
-        'Hold o to activate golem behavior (must be holding for attacks to effect them)\n' +
-        'Press y to die\n' +
-        'Press r to respawn',50, 100);
-    }
 
-    // Center the canvas around the player
-    translate(windowWidth / 2 - wizard.sprite.position.x, windowHeight / 2 - wizard.sprite.position.y);
+// var test;
+// var respawnState = false; // New variable to track respawn state
 
-    // Draw the player
-    wizard.sprite.draw();
+// function draw() {
+//     // state = 0 corresponds to start screen
+//     if (state == 0) {
+//         background("#5cb8ff");   // arbitrary color choice, can be changed
+//         fill(0);
+//         textSize(50);
+//         textAlign(CENTER);
+//         text("The Wizard's Quest", width / 2, height / 2);
+//         startButton.draw();
+//         wizard.sprite.changeAni(emptyAnim);
+//     }
+//     // state = 1 corresponds to level0
+//     if (state == 1) {
+//         background("#fce1b6");   // arbitrary color choice, can be changed
+//         if (level0Drawn == 0) { drawLevel0(); }   // makes sure level sprites only get drawn once - breaks otherwise
+//         textAlign(LEFT);
+//         text('WASD to move\n' +
+//             'Click to attack (mouse to aim)\n' +
+//             'Space to shoot fireball sideways\n' +
+//             'Press 1 to change attack\n' +
+//             'Press b to spawn golem enemy\n' +
+//             'Hold o to activate golem behavior (must be holding for attacks to effect them)\n' +
+//             'Press y to die\n' +
+//             'Press r to respawn', 50, 100);
+//     }
 
-    playerMovement();
+//     // Center the canvas around the player
+//     translate(windowWidth / 2 - wizard.sprite.position.x, windowHeight / 2 - wizard.sprite.position.y);
 
-    // press b to spawn golem in random pos
-    if (kb.presses('b')) {
-        let newGolem;
-        newGolem = new golem(Math.floor(Math.random() * 401), Math.floor(Math.random() * 401));
-    }
+//     // Draw the player
+//     wizard.sprite.draw();
 
-    // Normalize movement, so player does not move faster in diagonal movements
-    // see pyth. theorem
-    wizard.normalizeMovement();
-    if (state != 0) { castSpell(); }          // prevents spells from being cast on title screen
-    golemBehavior();
+//     playerMovement();
 
-    // Respawn player when 'r' is pressed
-    if (kb.presses('r') && !respawnState) {
-        respawnPlayer();
-        respawnState = true;
-    }
+//     // press b to spawn golem in random pos
+//     if (kb.presses('b')) {
+//         let newGolem;
+//         newGolem = new golem(Math.floor(Math.random() * 401), Math.floor(Math.random() * 401));
+//     }
 
-    // Reset respawn state when 'r' is released
-    if (kb.released('r')) {
-        respawnState = false;
-    }
-}
+//     // Normalize movement, so player does not move faster in diagonal movements
+//     // see pyth. theorem
+//     wizard.normalizeMovement();
+//     if (state != 0) { castSpell(); }          // prevents spells from being cast on title screen
+//     golemBehavior();
+
+//     // Respawn player when 'r' is pressed
+//     if (kb.presses('r') && !respawnState) {
+//         respawnPlayer();
+//         respawnState = true;
+//     }
+
+//     // Reset respawn state when 'r' is released
+//     if (kb.released('r')) {
+//         respawnState = false;
+//     }
+// }
